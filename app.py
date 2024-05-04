c0dccf798123297995a9520e06255725514d341f
VintellX <vintellx@gmail.com>
2024-05-04 19:24:37 +0530

    Add Google Authentication
    Implement Google authentication for automatic retrieval and verification of university email addresses.

--- app.py
+++ app.py
@@ -7,14 +7,15 @@
 # Why the hell am I using Flask? I don't know. I'm not even a Web Developer by profession. xD
 
 
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from flask_session import Session
+from authlib.integrations.flask_client import OAuth
 from pymongo import MongoClient
 import os
 from xDmongo import importo
 import re
 import redis
-
+import requests
 app = Flask(__name__)
 xdsecret: str= os.urandom(24).hex()
 app.secret_key = xdsecret 
@@ -27,13 +28,29 @@ redisp: str= os.environ.get('REDIS_PASS')
 redisport: int= os.environ.get('REDIS_PORT')
 app.config['SESSION_REDIS'] = redis.Redis(host=rediso,port=redisport,password=redisp)
 Session(app)
-
-# Initialize MongoDB
+GOOGLE_CLIENT_ID: str= os.environ.get('GOOGLE_CLIENT_ID')
+GOOGLE_CLIENT_SECRET: str= os.environ.get('GOOGLE_CLIENT_SECRET')
+GOOGLE_JWKS_URI = "https://www.googleapis.com/oauth2/v3/certs"
+GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
 MONGO_URI: str= os.environ.get('MONGO_URI')
 wtf = MongoClient(MONGO_URI)
 db = wtf.xData
 recruit = db.recruit
+oauth = OAuth(app)
 
+# Flask-OAuthlib Sucks!
+
+oauth.register(
+    name='google',
+    client_id=GOOGLE_CLIENT_ID,
+    client_secret=GOOGLE_CLIENT_SECRET,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params={'scope': 'email'},
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    userinfo_endpoint='https://www.googleapis.com/oauth2/v3/userinfo',
+    userinfo_compliance_fix=True,
+    jwks_uri=GOOGLE_JWKS_URI,
+)
 from prototype import UserData
 
 @app.route('/')
@@ -44,12 +61,40 @@ def index():
 def recruit():
     return render_template('recruitment.html')
 
+def googleProvider():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+@app.route('/recruitment/login')
+def _login():
+    return render_template('login.html')
+
+@app.route('/login')
+def login():
+    redirect_uri = url_for('authorized', _external=True)
+    nonce = str(os.urandom(24).hex())
+    session['nonce'] = nonce
+    return oauth.google.authorize_redirect(redirect_uri, nonce=nonce)
+
+@app.route('/authorized')
+def authorized():
+    token = oauth.google.authorize_access_token()
+    nonce = session.pop('nonce')
+    userinfo = oauth.google.parse_id_token(token, nonce=nonce)
+    email = userinfo['email']
+    if not re.search(r'@vitbhopal\.ac\.in$', email):
+        return redirect(url_for('login'))
+    session['email'] = email
+    return redirect(url_for('process'))
+
 @app.route('/recruitment/process', methods=['GET','POST'])
 def process():
+    email = session.get('email')
+    if not email:
+        return redirect(url_for('login'))
     if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        regnum = request.form['regnum']
+        name = request.form['name'].title()
+        email = session.get('email')
+        regnum = request.form['regnum'].upper()
         phone = request.form['phone']
         team = request.form['team']
         sellingpoint = request.form['sellingpoint']
@@ -77,7 +122,7 @@ def process():
         status = UserData.updato(name, email, regnum, phone, team, sellingpoint, linkedin, github, projectolink, resumelink, ques1, ques2, ques3)
         session['status'] = status
         return redirect(url_for('submission'))
-    return render_template('form.html')
+    return render_template('form.html', email=email)
 
 @app.route('/recruitment/submission')
 def submission():
@@ -85,4 +130,5 @@ def submission():
     status = session.get('status') or None
     log = session.get('log') or None
     print(name, status, log)
+    session.pop('google_token', None)
     return render_template('submission.html', name=name, status=status, log=log) if name else redirect(url_for('recruit'))
